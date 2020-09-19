4396d9956bd6a4faf79a554723aa1c86599b97cd
Juliette Rapala <jrapala@gmail.com>
2020-09-19 14:13:08 -0500

    feat: add typography styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,11 +4,13 @@ import 'normalize.css';
 import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
