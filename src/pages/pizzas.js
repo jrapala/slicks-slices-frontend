db3551ad8a4201bafb4ea143a8104dc9b5be6e9a
Juliette Rapala <jrapala@gmail.com>
2020-09-20 17:47:58 -0500

    feat: add PizzaQuery and PizzaList component

--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function PizzasPage() {
+import PizzaList from '../components/PizzaList';
+
+export default function PizzasPage({ data }) {
+  // No need to add loading states since everything is pre-populated before page loads
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! I'm the Pizzas page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+// dynamic page queries must happen at top level (page level)
+// static queries can happen anywhere
+//
+// Gatsby recongizes the exported query and passes it down as props
+export const pageQuery = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
