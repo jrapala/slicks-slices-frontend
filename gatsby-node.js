751d6e5a391d194284fb0e05ce2f785ac9eb9f01
Juliette Rapala <jrapala@gmail.com>
2020-09-24 20:49:43 -0500

    feat: fetch beer info from beer API and create GraphQL nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,6 @@
 import path from 'path';
+// fetch is part of the browser API--use this for Node
+import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
   // 1. Get a template for this page
@@ -55,6 +57,42 @@ async function turnToppingsIntoPages({ graphql, actions }) {
     });
   });
   // 4. Pass topping data to pizza.js
+}
+
+async function fetchBeersAndTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  console.log('Turn beers into nodes!');
+  // 1. Fetch a list of beers
+  const res = await fetch('https://sampleapis.com/beers/api/ale');
+  const beers = await res.json();
+  // 2. Loop over each one
+  for (const beer of beers) {
+    // 3. Create a note for that beer
+    const nodeMeta = {
+      // No ids in API response, so create your own
+      id: createNodeId(`beer-${beer.name}`),
+      // options to create relational data
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+    actions.createNode({
+      ...beer,
+      ...nodeMeta,
+    });
+  }
+}
+
+export async function sourceNodes(params) {
+  // fetch a list of beers and source them into our Gatsby API
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
 }
 
 export async function createPages(params) {
