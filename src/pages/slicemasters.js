2bba40037ed062b7bfd8299265b08bd70ead2dee
Juliette Rapala <jrapala@gmail.com>
2020-09-30 20:52:49 -0500

    feat: create Pagination component and add it to slicemasters pages

--- src/pages/slicemasters.js
+++ src/pages/slicemasters.js
@@ -3,6 +3,8 @@ import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
 
+import Pagination from '../components/Pagination';
+
 const SlicemasterGrid = styled.div`
   display: grid;
   grid-gap: 2rem;
@@ -36,11 +38,18 @@ const SlicemasterStyles = styled.div`
   }
 `;
 
-export default function SlicemastersPage({ data }) {
+export default function SlicemastersPage({ data, pageContext }) {
   const slicemasters = data.slicemasters.nodes;
 
   return (
     <>
+      <Pagination
+        pageSize={parseInt(process.env.GATSBY_PAGE_SIZE)}
+        totalCount={data.slicemasters.totalCount}
+        currentPage={pageContext.currentPage || 1}
+        skip={pageContext.skip}
+        base="/slicemasters"
+      />
       <SlicemasterGrid>
         {slicemasters.map((person) => (
           <SlicemasterStyles key={person.id}>
